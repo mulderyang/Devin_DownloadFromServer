721c13d77f95344ec08321cbba21dc79e9d3df56
jeny <iblind@naver.com>
2017-05-17 17:56:20 +0900

    Image Download from Network and then Save the bitmap into files directory.

---
+++ app/src/main/java/betheracer/devin_downloadfromserver/DownloadTask.java
@@ -0,0 +1,86 @@
+package betheracer.devin_downloadfromserver;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ * Created by jeny on 2017-05-17.
+ */
+public class DownloadTask extends AsyncTask<String, Void, Void> {
+
+    private Context mContext;
+    private String mUrl;
+    private Uri mOutputUri;
+
+    public DownloadTask(Context context, String urlStr) {
+
+        mContext = context;
+        mUrl = urlStr;
+    }
+
+    @Override
+    protected Void doInBackground(String... params) {
+
+        String phoneNo = params[0];
+
+        mUrl = mUrl + phoneNo + ".png";
+
+        mOutputUri = getOutputUri(phoneNo);
+
+        Bitmap bitmap;
+
+        try {
+            InputStream inputStream = (new URL(mUrl)).openStream();
+
+            bitmap = BitmapFactory.decodeStream(inputStream);
+
+            writeBitmapToUri(mContext, bitmap, mOutputUri);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+
+    private Uri getOutputUri(String phoneNo) {
+
+        Uri uri = null;
+
+        File file = mContext.getFilesDir();
+        String fileStr = file.toString();
+        fileStr = fileStr + File.separator + phoneNo + ".png";
+
+        File file2 = new File(fileStr);
+        uri = Uri.fromFile(file2);
+
+        return uri;
+    }
+
+
+    private void writeBitmapToUri(Context context, Bitmap bitmap, Uri uri) {
+
+        OutputStream outputStream = null;
+
+        try {
+            outputStream = context.getContentResolver().openOutputStream(uri);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
--- app/src/main/java/betheracer/devin_downloadfromserver/MainActivity.java
+++ app/src/main/java/betheracer/devin_downloadfromserver/MainActivity.java
@@ -1,13 +1,26 @@
 package betheracer.devin_downloadfromserver;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    //public String phoneNo = "01031287657";
+    public String phoneNo = "15555215554";  // phone number of emulator
+
+
+
+    public String urlStr = "http://192.168.0.16/shop/android/images/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        DownloadTask task = new DownloadTask(this, urlStr);
+        task.execute(phoneNo);
     }
+
 }
